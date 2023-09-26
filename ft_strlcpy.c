e9ab517156d0486097646eca7f1ae9a7fab91539
Kosuke Imai <koimai@c1r19s3.42tokyo.jp>
2023-09-26 19:13:32 +0900

    modified ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: koimai <koimai@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: koimai <koimai@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 15:21:51 by koimai            #+#    #+#             */
-/*   Updated: 2023/09/21 15:46:40 by koimai           ###   ########.fr       */
+/*   Updated: 2023/09/26 19:11:30 by koimai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,7 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t n)
 	size_t	src_len;
 	size_t	i;
 
-	src_len = 0;
-	while (src[src_len] != '\0')
-		src_len++;
+	src_len = ft_strlen(src);
 	if (n == 0)
 		return (src_len);
 	i = 0;
@@ -29,33 +27,5 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t n)
 		i++;
 	}
 	dest[i] = '\0';
-	if (src_len <= n)
-		return (src_len);
-	return (n - 1);
+	return (src_len);
 }
-
-// #include <stdio.h>
-// #include <string.h>
-// int main() {
-//     char dest[20];
-//     const char *src = "Hello, World!";
-//     size_t n;
-
-//     n = 0;
-//     size_t result1 = ft_strlcpy(dest, src, n);
-//     printf("Test 1: result=%zu, expected=%zu\n", result1, strlen(src));
-
-//     n = 30;
-//     size_t result2 = ft_strlcpy(dest, src, n);
-//     printf("Test 2: result=%zu, expected=%zu\n", result2, strlen(src));
-
-//     n = strlen(src);
-//     size_t result3 = ft_strlcpy(dest, src, n);
-//     printf("Test 3: result=%zu, expected=%zu\n", result3, strlen(src));
-
-//     n = 5;
-//     size_t result4 = ft_strlcpy(dest, src, n);
-//     printf("Test 4: result=%zu, expected=%zu\n", result4, n - 1);
-
-//     return 0;
-// }
