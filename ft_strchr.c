134c68488e9ffdd64a1d7b4c45a66429465d5670
Kosuke Imai <koimai@c1r19s3.42tokyo.jp>
2023-09-26 17:03:59 +0900

    modified ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: koimai <koimai@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: koimai <koimai@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 13:51:19 by koimai            #+#    #+#             */
-/*   Updated: 2023/09/21 14:15:13 by koimai           ###   ########.fr       */
+/*   Updated: 2023/09/26 17:03:17 by koimai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,14 @@
 char	*ft_strchr(const char *s, int c)
 {
 	unsigned int	i;
+	unsigned int	len;
 
+	c &= 255;
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	if (c == '\0' && s[len] == c)
+		return ((char *)&s[len]);
 	i = 0;
 	while (s[i] != '\0')
 	{
@@ -25,14 +32,3 @@ char	*ft_strchr(const char *s, int c)
 	}
 	return (NULL);
 }
-
-// #include <stdio.h>
-
-// int main(void)
-// {
-// 	char test[20] = "abcdefghijk";
-// 	char *p;
-// 	p = ft_strchr(test, 'h');
-// 	printf("検索文字が見つかった場所から表示: %s\n", p);
-// 	return (0);
-// }
