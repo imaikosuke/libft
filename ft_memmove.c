abed08a18f36c5969086ff40eb554d2dc815037e
Kosuke Imai <koimai@c1r19s3.42tokyo.jp>
2023-09-26 16:03:23 +0900

    modified ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -3,57 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: koimai <koimai@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: koimai <koimai@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 13:03:35 by koimai            #+#    #+#             */
-/*   Updated: 2023/09/22 17:17:16 by koimai           ###   ########.fr       */
+/*   Updated: 2023/09/26 16:00:58 by koimai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t len)
 {
-	unsigned char		*tmp;
-	const unsigned char	*s;
-	size_t				i;
+	unsigned char	*tmp;
+	unsigned char	*s;
+	size_t			i;
 
-	tmp = dest;
-	s = src;
-	if (dest <= src)
+	tmp = (unsigned char *)dest;
+	s = (unsigned char *)src;
+	if (dest > src)
 	{
-		i = -1;
-		while (++i < n)
-			tmp[i] = s[i];
+		while (len-- != 0)
+			tmp[len] = s[len];
 	}
 	else
 	{
-		tmp += n;
-		s += n;
-		i = n + 1;
-		while (0 < --i)
-			tmp[i - 1] = s[i - 1];
+		i = 0;
+		while (i < len)
+		{
+			tmp[i] = s[i];
+			i++;
+		}
 	}
 	return (dest);
 }
-
-// #include <stdio.h>
-// #include <string.h>
-// #define BUFSIZE 64
-// #define BEGIN 3
-// #define N 5
-// int main(void)
-// {
-//   char str[] = "0123456789";
-//   char s[BUFSIZE], t[BUFSIZE];
-//   size_t len;
-//   len = strlen(str) + 1;
-//   ft_memmove(s, str, len);
-//   ft_memmove(t, str, len);
-//   printf("ft_memmove(): s = %s, t = %s\n", s, t);
-//   ft_memmove(t + BEGIN, t, N);
-//   printf("ft_memmove(): t = %s\n", t);
-//   ft_memmove(t, t + BEGIN, N);
-//   printf("ft_memmove(): t = %s\n", t);
-//   return 0;
-// }
