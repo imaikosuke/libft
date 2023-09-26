cfa829d2c6c58b10284578fa922610ebb7068a2d
Kosuke Imai <koimai@c1r19s3.42tokyo.jp>
2023-09-26 18:22:36 +0900

    modified ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: koimai <koimai@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: koimai <koimai@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 15:48:13 by koimai            #+#    #+#             */
-/*   Updated: 2023/09/21 15:56:12 by koimai           ###   ########.fr       */
+/*   Updated: 2023/09/26 18:20:48 by koimai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,38 +16,26 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
+	if (n == 0)
+		return (0);
 	i = 0;
-	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && i < n)
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && i < n - 1)
 		i++;
-	return ((int)(s1 - s2));
+	if (0 < s1[i] - s2[i])
+		return (1);
+	else if (0 > s1[i] - s2[i])
+		return (-1);
+	else
+		return (0);
 }
 
 // #include <stdio.h>
-// #include <string.h>
-// int main() {
-//     const char *str1 = "Hello, World!";
-//     const char *str2 = "Hello, John!";
-//     size_t n;
-
-//     // テストケース1: 同じ文字列を比較
-//     n = strlen(str1);
-//     int result1 = ft_strncmp(str1, str1, n);
-//     printf("Test 1: result=%d, expected=0\n", result1);
-
-//     // テストケース2: 異なる文字列を比較 (同じ長さ)
-//     n = strlen(str1);
-//     int result2 = ft_strncmp(str1, str2, n);
-//     printf("Test 2: result=%d, expected=%d\n", result2, strcmp(str1, str2));
-
-//     // テストケース3: 部分的な一致 (nよりも長い)
-//     n = 10;
-//     int result3 = ft_strncmp(str1, str2, n);
-//     printf("Test 3: result=%d, expected=%d\n", result3, strcmp(str1, str2));
-
-//     // テストケース4: 部分的な一致 (nよりも短い)
-//     n = 5;
-//     int result4 = ft_strncmp(str1, str2, n);
-//     printf("Test 4: result=%d, expected=%d\n", result4, strcmp(str1, str2));
-
-//     return 0;
+// int main(void)
+// {
+// 	const char s1[] = "t";
+// 	const char s2[] = "";
+// 	size_t n = 0;
+// 	int result = ft_strncmp(s1, s2, n);
+// 	printf("result: %d\n", result);
+// 	return 0;
 // }
