b54da0c7c1f93727b409a978d6e0a4cc9e3669ad
Kosuke Imai <koimai@c1r18s6.42tokyo.jp>
2023-09-30 21:49:25 +0900

    modified ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,15 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: koimai <koimai@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: koimai <koimai@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 20:07:28 by koimai            #+#    #+#             */
-/*   Updated: 2023/09/29 01:22:22 by koimai           ###   ########.fr       */
+/*   Updated: 2023/09/30 21:47:23 by koimai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
+
 int	is_in_set(char c, char const *set)
 {
 	int	i;
@@ -30,7 +30,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	int		start;
 	int		end;
-	int		len;
 	int		trimmed_len;
 	char	*trimmed_str;
 
@@ -39,15 +38,10 @@ char	*ft_strtrim(char const *s1, char const *set)
 	start = 0;
 	while (s1[start] != '\0' && is_in_set(s1[start], set))
 		start++;
-	end = start;
-	len = 0;
-	while (s1[end] != '\0')
-	{
-		if (!is_in_set(s1[end], set))
-			len = end + 1;
-		end++;
-	}
-	trimmed_len = len - start;
+	end = ft_strlen(s1) - 1;
+	while (s1[end] && is_in_set(s1[end], set))
+		end--;
+	trimmed_len = end - start + 1;
 	if (trimmed_len <= 0)
 		return (ft_strdup(""));
 	trimmed_str = (char *)malloc((trimmed_len + 1) * sizeof(char));
