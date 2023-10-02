31b88a2f76f896bc02531e1d0976bd569063f709
imaikosuke <imaikosuke2@gmail.com>
2023-10-02 14:16:27 +0900

    modified ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: koimai <koimai@student.42.fr>              +#+  +:+       +#+        */
+/*   By: koimai <koimai@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 16:35:31 by koimai            #+#    #+#             */
-/*   Updated: 2023/09/26 17:40:45 by koimai           ###   ########.fr       */
+/*   Updated: 2023/10/02 14:15:57 by koimai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,14 @@
 char	*ft_strrchr(const char *str, int c)
 {
 	size_t	len;
+	char	ch;
 
-	c &= 255;
+	ch = (char)c;
 	len = ft_strlen(str) + 1;
 	while (0 < len)
 	{
 		len--;
-		if (str[len] == c)
+		if (str[len] == ch)
 			return ((char *)&str[len]);
 	}
 	return (NULL);
