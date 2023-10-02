d5b9832ab2ef33b6e2a19a5d559ea8fccaa24d44
imaikosuke <imaikosuke2@gmail.com>
2023-10-02 15:13:54 +0900

    modified ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: koimai <koimai@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 15:56:16 by koimai            #+#    #+#             */
-/*   Updated: 2023/09/21 16:35:20 by koimai           ###   ########.fr       */
+/*   Updated: 2023/10/02 14:32:38 by koimai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,34 +35,3 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	}
 	return (NULL);
 }
-
-// #include <stdio.h>
-// int main() {
-//     const char *haystack = "This is a test string.";
-//     const char *needle1 = "test";
-//     const char *needle2 = "is";
-//     const char *needle3 = "xyz";
-//     const char *needle4 = "string.";
-
-//     // テストケース1: haystack内でneedle1を見つける
-//     char *result1 = ft_strnstr(haystack, needle1, 20);
-//     printf("Test 1: result=\"%s\"\n", result1);
-
-//     // テストケース2: haystack内でneedle2を見つける
-//     char *result2 = ft_strnstr(haystack, needle2, 20);
-//     printf("Test 2: result=\"%s\"\n", result2);
-
-//     // テストケース3: haystack内でneedle3は見つからない
-//     char *result3 = ft_strnstr(haystack, needle3, 20);
-//     printf("Test 3: result=\"%s\"\n", result3);
-
-//     // テストケース4: haystack内でneedle4を見つける
-//     char *result4 = ft_strnstr(haystack, needle4, 22);
-//     printf("Test 4: result=\"%s\"\n", result4);
-
-//     // テストケース5: needle1が空の場合、haystackの先頭を返す
-//     char *result5 = ft_strnstr(haystack, "", 20);
-//     printf("Test 5: result=\"%s\"\n", result5);
-
-//     return 0;
-// }
