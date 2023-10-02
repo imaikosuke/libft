718d061a59995ff1655ad1f828e5a8219f200ba7
imaikosuke <imaikosuke2@gmail.com>
2023-10-02 15:28:26 +0900

    modified ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: koimai <koimai@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 20:06:23 by koimai            #+#    #+#             */
-/*   Updated: 2023/09/22 19:28:21 by koimai           ###   ########.fr       */
+/*   Updated: 2023/10/02 15:27:28 by koimai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	size_t	s1_len;
 	size_t	s2_len;
-	char	*p;
+	char	*new_str;
 
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
-	p = malloc(s1_len + s2_len + 1);
-	ft_memcpy(p, s1, s1_len);
-	ft_memcpy(p + s1_len, s2, s2_len + 1);
-	return (p);
+	new_str = (char *)malloc((s1_len + s2_len + 1) * sizeof(char));
+	ft_memcpy(new_str, s1, s1_len);
+	ft_memcpy(new_str + s1_len, s2, s2_len + 1);
+	return (new_str);
 }
