7213aac172a5a06b916bac7463fb14ac3428a067
Kosuke Imai <koimai@c1r18s7.42tokyo.jp>
2023-10-03 18:22:21 +0900

    modified ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: koimai <koimai@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 12:12:56 by koimai            #+#    #+#             */
-/*   Updated: 2023/10/03 17:56:10 by koimai           ###   ########.fr       */
+/*   Updated: 2023/10/03 18:21:24 by koimai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 
 	ch = (unsigned char)c;
 	str = (unsigned char *)s;
-	if (str == NULL)
+	if (str == NULL && n <= 0)
 		return (NULL);
 	while (n--)
 	{
