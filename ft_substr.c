4f01929cc8b8d7962336dc26f672303033b228dc
Kosuke Imai <koimai@c1r18s7.42tokyo.jp>
2023-10-03 19:28:43 +0900

    modified ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: koimai <koimai@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 18:52:15 by koimai            #+#    #+#             */
-/*   Updated: 2023/10/03 18:43:19 by koimai           ###   ########.fr       */
+/*   Updated: 2023/10/03 19:28:06 by koimai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	i;
 	size_t	size;
 
+	if (!s)
+		return (NULL);
 	size = ft_strlen(s);
 	if (size < start)
 		return (ft_strdup(""));
