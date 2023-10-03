0bc261324274002b027078fee403c960f1c91cf0
Kosuke Imai <koimai@c1r18s7.42tokyo.jp>
2023-10-03 15:31:51 +0900

    modified norminette

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -3,23 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: koimai <koimai@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: koimai <koimai@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 22:29:45 by koimai            #+#    #+#             */
-/*   Updated: 2023/10/01 14:28:36 by koimai           ###   ########.fr       */
+/*   Updated: 2023/10/03 15:05:17 by koimai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstnew(void *content)
+t_list	*ft_lstnew(void *content)
 {
-    t_list *new_node;
-    
-    new_node = (t_list *)malloc(sizeof(t_list));
-    if (new_node == NULL)
-        return (NULL);
-    new_node->content = content;
-    new_node->next = NULL;
-    return (new_node);
+	t_list	*new_node;
+
+	new_node = (t_list *)malloc(sizeof(t_list));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->content = content;
+	new_node->next = NULL;
+	return (new_node);
 }
