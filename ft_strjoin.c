fc408e053a5400accf2197aa2e340d6845bce44b
Kosuke Imai <koimai@c1r18s7.42tokyo.jp>
2023-10-04 13:51:52 +0900

    modified ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: koimai <koimai@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 20:06:23 by koimai            #+#    #+#             */
-/*   Updated: 2023/10/03 18:40:43 by koimai           ###   ########.fr       */
+/*   Updated: 2023/10/04 13:51:03 by koimai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	s2_len;
 	char	*new_str;
 
+	if (!s1 || !s2)
+		return (NULL);
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
 	new_str = (char *)malloc((s1_len + s2_len + 1) * sizeof(char));
