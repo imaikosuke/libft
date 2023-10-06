919156abaaea3a9ad3196811fafdb254737c4e16
Kosuke Imai <koimai@c1r19s3.42tokyo.jp>
2023-10-06 20:50:01 +0900

    modified ft_lstmap.c

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,34 +6,49 @@
 /*   By: koimai <koimai@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 14:53:03 by koimai            #+#    #+#             */
-/*   Updated: 2023/10/03 15:30:00 by koimai           ###   ########.fr       */
+/*   Updated: 2023/10/06 20:42:58 by koimai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_clear(t_list *node, t_list *list, void (*del)(void *), int n)
+{
+	if (!node && n == 1)
+	{
+		ft_lstclear(&list, del);
+		return (1);
+	}
+	else if (!node && n == 2)
+	{
+		free(node);
+		ft_lstclear(&list, del);
+		return (1);
+	}
+	return (0);
+}
+
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
 	t_list	*new_node;
-	t_list	**last;
+	t_list	**tmp;
 
+	if (!lst || !f || !del)
+		return (NULL);
 	new_list = NULL;
-	last = &new_list;
+	tmp = &new_list;
 	while (lst)
 	{
 		new_node = (t_list *)malloc(sizeof(t_list));
-		new_node->content = f(lst->content);
-		if (!new_node || !new_node->content)
-		{
-			if (new_node)
-				free(new_node);
-			ft_lstclear(&new_list, del);
+		if (ft_clear(new_node, new_list, del, 1))
 			return (NULL);
-		}
+		new_node->content = f(lst->content);
+		if (ft_clear(new_node, new_list, del, 2))
+			return (NULL);
 		new_node->next = NULL;
-		*last = new_node;
-		last = &new_node->next;
+		*tmp = new_node;
+		tmp = &new_node->next;
 		lst = lst->next;
 	}
 	return (new_list);
