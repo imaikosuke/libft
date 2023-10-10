b957242df6881db4709d0b71603e57ee0f2b8536
Kosuke Imai <koimai@c1r27s8.42tokyo.jp>
2023-10-10 16:46:22 +0900

    modified ft_memmove.c ft_memset.c ft_strchr.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,23 +6,23 @@
 /*   By: koimai <koimai@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 13:03:35 by koimai            #+#    #+#             */
-/*   Updated: 2023/10/10 16:19:11 by koimai           ###   ########.fr       */
+/*   Updated: 2023/10/10 16:44:06 by koimai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dest, const void *src, size_t len)
 {
 	unsigned char	*tmp;
 	unsigned char	*s;
 	size_t			i;
 
-	tmp = (unsigned char *)dst;
+	tmp = (unsigned char *)dest;
 	s = (unsigned char *)src;
 	if (!tmp && !s)
 		return (NULL);
-	if (dst > src)
+	if (dest > src)
 	{
 		while (len--)
 			tmp[len] = s[len];
@@ -36,5 +36,5 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 			i++;
 		}
 	}
-	return (dst);
+	return (dest);
 }
--- ft_memset.c
+++ ft_memset.c
@@ -6,25 +6,25 @@
 /*   By: koimai <koimai@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 13:37:25 by koimai            #+#    #+#             */
-/*   Updated: 2023/10/10 16:20:21 by koimai           ###   ########.fr       */
+/*   Updated: 2023/10/10 16:44:36 by koimai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *b, int c, size_t len)
+void	*ft_memset(void *s, int c, size_t n)
 {
 	unsigned char	*str;
 	size_t			index;
 	unsigned char	ch;
 
-	str = (unsigned char *)b;
+	str = (unsigned char *)s;
 	ch = (unsigned char)c;
 	index = 0;
-	while (index < len)
+	while (index < n)
 	{
 		str[index] = ch;
 		index++;
 	}
-	return (b);
+	return (s);
 }
