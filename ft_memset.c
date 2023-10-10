78f216a429fd9578d34274fc66e0d207ca752e81
Kosuke Imai <koimai@c1r27s8.42tokyo.jp>
2023-10-10 15:46:06 +0900

    modified ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: koimai <koimai@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 13:37:25 by koimai            #+#    #+#             */
-/*   Updated: 2023/10/10 15:22:21 by koimai           ###   ########.fr       */
+/*   Updated: 2023/10/10 15:41:09 by koimai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	*ft_memset(void *s, int c, size_t n)
 {
-	char	*str;
-	size_t	index;
-	char	ch;
+	unsigned char	*str;
+	size_t			index;
+	unsigned char	ch;
 
-	str = s;
-	ch = (char)c;
+	str = (unsigned char *)s;
+	ch = (unsigned char)c;
 	index = 0;
 	while (index < n)
 	{
