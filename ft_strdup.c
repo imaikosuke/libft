06c343021edbe9d6b77a8445c92af002e2601298
Kosuke Imai <koimai@c1r27s8.42tokyo.jp>
2023-10-10 16:38:58 +0900

    modified ft_strdup.c libft.h

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,28 +6,28 @@
 /*   By: koimai <koimai@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 14:15:20 by koimai            #+#    #+#             */
-/*   Updated: 2023/10/10 16:21:46 by koimai           ###   ########.fr       */
+/*   Updated: 2023/10/10 16:37:48 by koimai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(const char *s1)
+char	*ft_strdup(const char *str)
 {
 	size_t	index;
 	size_t	len;
 	char	*duplicate;
 
 	len = 0;
-	while (s1[len] != '\0')
+	while (str[len] != '\0')
 		len++;
 	duplicate = malloc((len + 1) * sizeof(char));
 	if (duplicate == NULL)
 		return (NULL);
 	index = 0;
-	while (s1[index] != '\0')
+	while (str[index] != '\0')
 	{
-		duplicate[index] = s1[index];
+		duplicate[index] = str[index];
 		index++;
 	}
 	duplicate[index] = '\0';
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: koimai <koimai@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 11:29:09 by koimai            #+#    #+#             */
-/*   Updated: 2023/10/03 15:14:31 by koimai           ###   ########.fr       */
+/*   Updated: 2023/10/10 16:37:15 by koimai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t num_elements, size_t element_size);
-char	*ft_strdup(char *src);
+char	*ft_strdup(const char *src);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
